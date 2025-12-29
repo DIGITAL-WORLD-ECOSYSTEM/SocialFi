db3c8dee5e49b2df4ed848b8fdd064ce59411fd7
associaasppibra <associaasppibra@gmail.com>
2025-12-29 23:43:29 +0000

    Initial commit

---
+++ src/sections/home/view/home-view.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Stack from '@mui/material/Stack';
+
+import { BackToTopButton } from 'src/components/animate/back-to-top-button';
+import { ScrollProgress, useScrollProgress } from 'src/components/animate/scroll-progress';
+
+import { HomeHero } from '../home-hero';
+import { HomeFAQs } from '../home-faqs';
+import { HomeIntegrations } from '../home-integrations';
+import { HomeAdvertisement } from '../home-advertisement';
+
+// ----------------------------------------------------------------------
+
+export function HomeView() {
+  const pageProgress = useScrollProgress();
+
+  return (
+    <>
+      <ScrollProgress
+        variant="linear"
+        progress={pageProgress.scrollYProgress}
+        sx={[(theme) => ({ position: 'fixed', zIndex: theme.zIndex.appBar + 1 })]}
+      />
+
+      <BackToTopButton />
+
+      <HomeHero />
+
+      <Stack sx={{ position: 'relative', bgcolor: 'background.default' }}>
+        <HomeIntegrations />
+
+        <HomeFAQs />
+
+        <HomeAdvertisement />
+      </Stack>
+    </>
+  );
+}
