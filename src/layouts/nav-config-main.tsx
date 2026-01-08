6b7e8d3d3e86792927a55ad9fbdb17330deece30
associaasppibra <associaasppibra@gmail.com>
2026-01-08 00:35:40 +0000

    feat: Atualiza header da home

--- src/layouts/nav-config-main.tsx
+++ src/layouts/nav-config-main.tsx
@@ -2,67 +2,14 @@ import type { NavMainProps } from './main/nav/types';
 
 import { paths } from 'src/routes/paths';
 
-import { CONFIG } from 'src/global-config';
-
-import { Iconify } from 'src/components/iconify';
-
 // ----------------------------------------------------------------------
 
 export const navData: NavMainProps['data'] = [
-  { title: 'Home', path: '/', icon: <Iconify width={22} icon="solar:home-angle-bold-duotone" /> },
-  {
-    title: 'Components',
-    path: paths.components,
-    icon: <Iconify width={22} icon="solar:atom-bold-duotone" />,
-  },
-  {
-    title: 'Pages',
-    path: '/pages',
-    icon: <Iconify width={22} icon="solar:file-bold-duotone" />,
-    children: [
-      {
-        subheader: 'Other',
-        items: [
-          { title: 'About us', path: paths.about },
-          { title: 'Contact us', path: paths.contact },
-          { title: 'FAQs', path: paths.faqs },
-          { title: 'Pricing', path: paths.pricing },
-          { title: 'Payment', path: paths.payment },
-          { title: 'Maintenance', path: paths.maintenance },
-          { title: 'Coming soon', path: paths.comingSoon },
-        ],
-      },
-      {
-        subheader: 'Concepts',
-        items: [
-          { title: 'Posts', path: paths.post.root },
-          { title: 'Post', path: paths.post.demo.details },
-        ],
-      },
-      {
-        subheader: 'Auth',
-        items: [
-          { title: 'Sign in', path: paths.auth.signIn },
-          { title: 'Sign up', path: paths.auth.signUp },
-          { title: 'Reset password', path: paths.auth.reset },
-          { title: 'Update password', path: paths.auth.update },
-          { title: 'Verify', path: paths.auth.verify },
-        ],
-      },
-      {
-        subheader: 'Error',
-        items: [
-          { title: 'Page 403', path: paths.page403 },
-          { title: 'Page 404', path: paths.page404 },
-          { title: 'Page 500', path: paths.page500 },
-        ],
-      },
-      { subheader: 'Dashboard', items: [{ title: 'Dashboard', path: CONFIG.auth.redirectPath }] },
-    ],
-  },
-  {
-    title: 'Docs',
-    icon: <Iconify width={22} icon="solar:notebook-bold-duotone" />,
-    path: paths.docs,
-  },
+  { title: 'Home', path: '/' },
+  { title: 'Ecosystem', path: '/ecosystem' },
+  { title: 'Community', path: '/community' },
+  { title: 'Team', path: paths.about },
+  { title: 'Roadmap', path: '/roadmap' },
+  { title: 'FAQ', path: paths.faqs },
+  { title: 'Blog', path: paths.post.root },
 ];
